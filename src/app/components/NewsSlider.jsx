058f25eea4749cfafec312a9cf4737fd26e13fb0
cihankarimli <kerimlicihan1@gmail.com>
2025-07-16 02:59:42 +0400

    added

--- src/app/components/NewsSlider.jsx
+++ src/app/components/NewsSlider.jsx
@@ -1,7 +1,7 @@
 "use client";
 import styles from "../styles/NewsSection.module.css";
 import React from "react";
-import News from "../data/news";
+import News from "../data/news.js";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { Swiper, SwiperSlide } from "swiper/react";
